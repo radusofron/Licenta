6d9aae4e430b2632044de83f8385515fd2745129
radusofron <radu.sofron169@gmail.com>
2023-06-10 19:17:43 +0300

    Few changes for itineraries FRONT END

--- webapp/static/scripts/itineraries.js
+++ webapp/static/scripts/itineraries.js
@@ -1,26 +1,34 @@
 // Start after HTML code is rendered
 window.addEventListener("load", function() {
     // Get every open button
-    const itineraries = document.querySelectorAll(".itineraries__open-button")
+    const buttons = document.querySelectorAll(".itineraries__open-button")
+
+    // Get every itinerary text container
+    const containers = document.querySelectorAll(".itineraries__itinerary-text")
 
     let number = 0
     // Add different backgrounds.
-    itineraries.forEach(itinerary => {
+    for (let index = 0; index <= buttons.length; index++) {
         if (number % 5 == 0) {
-            itinerary.style.backgroundColor = "#608DB8"
+            buttons[index].style.backgroundColor = "#608DB8"
+            containers[index].style.borderRightColor = "#608DB8"
         }
         if (number % 5 == 1) {
-            itinerary.style.backgroundColor = "#7EBDC2"
+            buttons[index].style.backgroundColor = "#7EBDC2"
+            containers[index].style.borderRightColor = "#7EBDC2"
         }
         if (number % 5 == 2) {
-            itinerary.style.backgroundColor = "#8C98AD"
+            buttons[index].style.backgroundColor = "#8C98AD"
+            containers[index].style.borderRightColor = "#8C98AD"
         }
         if (number % 5 == 3) {
-            itinerary.style.backgroundColor = "#C4A4A4"
+            buttons[index].style.backgroundColor = "#C4A4A4"
+            containers[index].style.borderRightColor = "#C4A4A4"
         }
         if (number % 5 == 4) {
-            itinerary.style.backgroundColor = "#D47E73"
+            buttons[index].style.backgroundColor = "#D47E73"
+            containers[index].style.borderRightColor = "#D47E73"
         }
         number++
-    });
+    }
   });
